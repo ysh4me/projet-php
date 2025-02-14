22fc479daf5bf3002bb22826ad7d0564a92ded01
Chaimaa Madhkour <cmadhkour@myges.fr>
2025-02-14 22:11:31 +0100

    Refonte de la page d'accueil avec ajout de sections pour les histoires, le partage d'images et l'inscription à la newsletter, ainsi que mise à jour de la barre de navigation et du pied de page.

--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -1,13 +1,22 @@
-<nav>
-    <ul>
-        <li><a href="/">Accueil</a></li>
-        <?php if (!empty($_SESSION['user']) && isset($_SESSION['user']['firstname'])): ?>
-            <li>Bienvenue, <?= htmlspecialchars($_SESSION['user']['firstname']) ?> !</li>
-            <li><a href="/logout">Déconnexion</a></li>
-        <?php else: ?>
-            <li><a href="/login">Connexion</a></li>
-            <li><a href="/register">Inscription</a></li>
-        <?php endif; ?>
+<div class="container">
+    <nav class="navbar">
+        <div class="navbar-brand">BuddyShotz</div>
+        <ul class="nav-links">
+            <li><a href="/">Accueil</a></li>
+            <?php if (!empty($_SESSION['user']) && isset($_SESSION['user']['firstname'])): ?>
+                <li><a href="#">Feed</a></li>
+                <li><a href="#">Upload</a></li>
+                <li><a href="#">Publish</a></li>
+                <li><a href="/logout">Déconnexion</a></li>
+            <?php else: ?>
+                <li><a href="#">Feed</a></li>
+                <li><a href="#">Upload</a></li>
+                <li><a href="#">Publish</a></li>
+                <li><a href="/login">Connexion</a></li>
+                <li><a href="/register">Inscription</a></li>
+            <?php endif; ?>
 
-    </ul>
-</nav>
+        </ul>
+    </nav>
+</div>
+
